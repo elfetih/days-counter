aa0c560eb5c6bd13083892c9aad7e5f6f2f4faf4
Kunal <kunaldhir@live.in>
2014-02-26 18:21:11 +0530

    geting input

---
+++ day-counter.c
@@ -0,0 +1,28 @@
+#include <math.h>
+
+void main()
+{
+int;
+float;
+clrscr();
+
+printf("/nenter date :");
+scanf("%d", &d1);
+printf("/nenter month number :");
+scanf("%d, &m1");
+printf("/nenter year :");
+scanf("%d", &y1);
+
+printf("/nnow give us the details of the other date below")
+
+printf("/nenter date :");
+scanf("%d", &d2);
+printf("/nenter month number :");
+scanf("%d, &m2");
+printf("/nenter year :");
+scanf("%d", &y1);
+
+
+
+getch();
+}
