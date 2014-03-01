f5740fa3826c29218fdfb25f53b8e3f7a9dcc29c
Kunal <kunaldhir@live.in>
2014-03-01 16:13:03 +0530

    Update day-counter.c

--- day-counter.c
+++ day-counter.c
@@ -2,8 +2,7 @@
 
 void main()
 {
-int d1,m1,y1,d2,m2,y2;
-float;
+int d1,m1,y1,d2,m2,y2,a1,a2,z,x,y,b1,b2,c1,c2,k,l;
 clrscr();
 
 printf("/nenter date :");
@@ -22,7 +21,15 @@ scanf("%d, &m2");
 printf("/nenter year :");
 scanf("%d", &y2);
 
+if(m1==1||3||5||7||8||1012) {a1=31-d1;}
+if(m1==2 && y1%4==0) {a1=29-d1;}
+if(m1==2 && y1%4==0) {a1=28-d1;}
+if(m1==4||7||9||11) {a1=30-d1;}
 
+b1=m1+1;
+c1=(x*31)+(y*30)+z;
+b2=m2-1;
+c2=(x*31)+(y*30)+z;
 
 
 getch();
